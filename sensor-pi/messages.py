c02599f32a11d287520c19114e8373f72b28e73c
Matteo Golin <matteo.golin@gmail.com>
2024-03-06 10:43:51 -0500

    feat: UDP messages are implemented using a SystemNode class to represent nodes on the system. no UDP communication is yet established

---
+++ sensor-pi/messages.py
@@ -0,0 +1,22 @@
+from enum import IntEnum
+import socket
+
+
+class Messages(IntEnum):
+    """Describes the possible messages that can be sent over UDP to other nodes."""
+
+    EMERGENCY = 0
+    NO_EMERGENCY = 1
+
+
+class SystemNode:
+    """Represents a device/node in the FANS system."""
+
+    def __init__(self, ip_addr: str, port: int) -> None:
+        self.ip_addr: str = ip_addr
+        self.port: int = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a new UDP socket
+
+    def send_message(self, message: Messages) -> None:
+        """Sends a message to the system node."""
+        self.socket.sendto(bytes(message), (self.ip_addr, self.port))
