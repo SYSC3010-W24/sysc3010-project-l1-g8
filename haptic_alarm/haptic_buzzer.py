574a67c2eb21b9b58e5ba04ab23c9bf8e1aada51
javeriasohail123 <146361273+javeriasohail123@users.noreply.github.com>
2024-03-07 01:44:21 -0300

    docs: Create haptic_buzzer.py

---
+++ haptic_alarm/haptic_buzzer.py
@@ -0,0 +1,39 @@
+import time
+import pyrebase
+import RPi.GPIO as GPIO
+
+# Firebase configuration
+config = { 
+  "apiKey": "AIzaSyDCrm-YWek1mShoftACTezFdzn8PoLSNrY", 
+  "databaseURL": "https://fans-38702-default-rtdb.firebaseio.com/"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+buzzer_pin = 17  # assuming I use pin 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzer_pin, GPIO.OUT)
+
+def control_alarm():
+    while True:
+        emergency_flag = db.child("emergency_flag").get().val()
+
+        if emergency_flag:
+            # Start buzzing
+            GPIO.output(buzzer_pin, GPIO.HIGH)
+            print("Emergency! Haptic alarm buzzing.")
+
+        else:
+            # Stop buzzing
+            GPIO.output(buzzer_pin, GPIO.LOW)
+            print("Emergency resolved. Haptic alarm stopped.")
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    try:
+        control_alarm()
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
