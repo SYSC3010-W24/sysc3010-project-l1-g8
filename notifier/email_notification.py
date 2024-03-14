07334da9dff6559067dc34f4ac685ca4aa5e848b
SajaFawagreh <115193073+SajaFawagreh@users.noreply.github.com>
2024-03-14 13:03:15 -0400

    feat: Update email_notification.py to have separate functions

--- notifier/email_notification.py
+++ notifier/email_notification.py
@@ -3,66 +3,77 @@ import ssl
 import smtplib
 import pyrebase 
 import time
+import json
+from templates import Template
 from datetime import datetime
 
-def sendemail(to_emails: list) -> None:
-    # Define sender's email and password
-    FROM = 'sysc3010.l1g8@gmail.com'
-    PASSWORD = 'iahv wbgh zxmt obdt'
+def createEmail(toEmailAddress: str, fromEmailAddress: str):
+
+    try:
+        user_name = toEmailAddress.split('@')[0]
+        user_name = user_name.replace('.', ' ')
+        user_name = user_name.title()
+    except IndexError:
+        user_name = toEmailAddress.split('@')[0]
     
-    for TO in to_emails:
-        # Attempt to extract user name from the email address
-        try:
-            user_name = TO.split('@')[0]
-            user_name = user_name.replace('.', ' ')
-            user_name = user_name.title()
-        except IndexError:
-            user_name = TO.split('@')[0]  # Default name if parsing fails
+    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    message = Template.from_file("./templates/emergency.txt")
+    fields = {"[DATE-TIME]": str(current_datetime), "[USER]": user_name}
+    
+    message.customize(fields)
+    
+    subject = "HIGH PRIORITY: Fire Alarm Emergency Notification"
+    
+    em = EmailMessage()
+    
+    em['From'] = fromEmailAddress
+    em['To'] = toEmailAddress
+    em['Subject'] = subject
+    em.set_content(str(message))    
+    
+    return em
+
+def sendemail(emailMessage, email, password):
+    context = ssl.create_default_context()
+    smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context)
+    smtp.login(email, password)
+    smtp.send_message(emailMessage)
         
-        # Email subject and body
-        SUBJECT = "HIGH PRIORITY: Fire Alarm Emergency Notification"
-        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        BODY = f"Dear {user_name},\nThis is an emergency notification. Please exit the building.\nEmergency detected: {current_datetime}"
+def connectFirebase(config: dict):
+    firebase = pyrebase.initialize_app(config) 
+    db = firebase.database() 
+    return db
         
-        # Prepare the email message
-        em = EmailMessage()
-        em['From'] = FROM
-        em['To'] = TO  # This needs to be a single email address string, not a list
-        em['Subject'] = SUBJECT
-        em['X-Priority'] = '1'  # High priority
-        em.set_content(BODY)
-        
-        # Create secure SSL connection for sending emails
-        context = ssl.create_default_context()
-        
-        # Sending the email
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            smtp.login(FROM, PASSWORD)
-            smtp.send_message(em)
-            
-        print('Email sent to ' + TO)
+def getEmergencyValue(db) -> bool:
+    emergencyValue = db.child('emergency').get().val()
+    return emergencyValue
 
 def main() -> None:
+
+    with open("./fans_credentials.json", "r") as file:
+        credentials: dict[str, str] = json.load(file)
+        
     # Firebase configuration
     config = {
         "apiKey": "AIzaSyDCrm-YWek1mShoftACTezFdzn8PoLSNrY",     
         "authDomain": "fans-38702.firebaseapp.com",     
         "databaseURL": "https://fans-38702-default-rtdb.firebaseio.com/",     
         "storageBucket": "fans-38702.appspot.com" 
-    }
+    }     
+        
+    db = connectFirebase(config)
     
-    # Connect to Firebase using the configuration 
-    firebase = pyrebase.initialize_app(config) 
-    db = firebase.database()     
-    
+    emails = ['saja.fawagreh@gmail.com']
+
     # Constantly poll for emergency
-    while True:
-        emergencyValue = db.child('emergency').get().val()
-        print(emergencyValue)
+    while True:    
+        emergencyValue = getEmergencyValue(db)
         while emergencyValue:
-            to_emails = ['saja.fawagreh@gmail.com', 'javeria1228@gmail.com']
-            sendemail(to_emails)
-            time.sleep(1)
+            for email in emails:
+                emailMessage = createEmail(email, credentials["email"])
+                sendemail(emailMessage, credentials["email"], credentials["pass"])
+                print("Email sent to " + email)
+                time.sleep(1)
 
         time.sleep(1)  # Poll DB every second
 
