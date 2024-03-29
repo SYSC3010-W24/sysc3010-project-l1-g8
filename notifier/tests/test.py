c042923c1ade38c9363b5ca3efd8a1a5869b873b
SajaFawagreh <115193073+SajaFawagreh@users.noreply.github.com>
2024-03-29 15:16:14 -0400

    feat: testing sending a message

---
+++ notifier/tests/test.py
@@ -0,0 +1,8 @@
+from messages import Messages
+import socket
+
+# Create the receiving socket
+ip_addr = ni.ifaddresses("wlan0")[ni.AF_INET][0]["addr"]
+channel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+channel.bind((ip_addr, 2003))
+socket.sendto(bytes(Messages.EMERGENCY), (ip_addr, 2003))
