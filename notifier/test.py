04a78001d1f22c197f20292f6062cc633a73c904
SajaFawagreh <115193073+SajaFawagreh@users.noreply.github.com>
2024-03-29 15:23:44 -0400

    Update test.py

--- notifier/test.py
+++ notifier/test.py
@@ -6,4 +6,4 @@ import netifaces as ni
 ip_addr = ni.ifaddresses("wlan0")[ni.AF_INET][0]["addr"]
 channel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 channel.bind((ip_addr, 2993))
-socket.sendto(bytes(Messages.EMERGENCY), (ip_addr, 2003))
+channel.sendto(bytes(Messages.EMERGENCY), (ip_addr, 2003))
