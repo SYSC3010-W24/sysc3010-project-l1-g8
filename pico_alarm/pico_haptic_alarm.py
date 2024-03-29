b23d89f32f2a42952dd210494b760502750383c4
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-03-28 21:22:45 -0400

    feat: Add UNFINISHED code for working with the pico w

--- pico_alarm/pico_haptic_alarm.py
+++ pico_alarm/pico_haptic_alarm.py
@@ -1,28 +1,84 @@
 import machine
-import time
+import utime
+import urequests as requests
+import json
+from picozero import LED
 
-buzzer_pin = machine.Pin(15, machine.Pin.OUT)
+# Connects buzzer to PicoW
+buzzer_pin = machine.Pin(15)
+buzzer_pwm = machine.PWM(buzzer_pin)
 
-def beep():
-    buzzer_pin.value(1)
-    time.sleep(0.1)  
-    buzzer_pin.value(0)
-    time.sleep(0.1)  
+# Connects LEDs to PicoW
+green_led = LED(11)
+red_led = LED(12)
 
-def haptic_alarm():
-    while True:
+# Connects button to PicoW
+stop_button = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_UP)
 
-        for _ in range(3):
-            beep()
-            time.sleep(0.2)  
-        time.sleep(1)
+# Frequencies (Hz) for alarm sound 
+low_freq = 400
+high_freq = 1000
 
-        for _ in range(3):
-            beep()
-            time.sleep(0.5)  
+# Amount of time alarm sound plays
+last_emergency_time = 0
+emergency_duration = 0
+emergency_threshold = 30000  # 30 seconds
 
-        time.sleep(1)
+def connectFirebase(config: dict):
+    firebase_url = config["databaseURL"] + "/emergency.json"  # Firebase URL for the emergency flag
+    return firebase_url
+
+def control_alarm(emergency_flag):
+    global buzzer_pwm
+    if emergency_flag:
+        # Start buzzing
+        update_led()
+        for _ in range(200):  # Play the alarm sound for 200 milliseconds
+            buzzer_pwm.freq(low_freq)
+            buzzer_pwm.duty_u16(32768)  # 50% duty cycle
+            utime.sleep_ms(100)
+        
+            buzzer_pwm.freq(high_freq)
+            utime.sleep_ms(100)
+        print("Emergency! Haptic alarm buzzing.")
+    else:
+        # Stop buzzing
+        buzzer_pwm.duty_u16(0)
+        update_led()
+        print("Emergency resolved. Haptic alarm stopped.")
+
+def update_led():
+    global last_emergency_time, emergency_duration, emergency_threshold
+    current_time = utime.ticks_ms()
+    
+    if stop_button.value() == 1 and current_time - last_emergency_time > emergency_duration:
+        red_led.off()
+        green_led.on()
+        
+    elif stop_button.value() == 1 and current_time - last_emergency_time <= emergency_duration:
+        red_led.blink(on_time=0.5, off_time=0.5)  # Blink red LED
+        
+    else:
+        red_led.on()  # Turn on red LED
 
 if __name__ == "__main__":
-    haptic_alarm()
+    # Firebase configuration
+    config = {"databaseURL": "https://fans-38702-default-rtdb.firebaseio.com/"}
+    firebase_url = connectFirebase(config)
+
+    # Main loop
+    green_led.on() # Turn on green led by default
+    red_led.off()
+    
+    while True:
+        try:
+            response = requests.get(firebase_url)
+            if response.status_code == 200:
+                emergency_flag = json.loads(response.text)
+                print(emergency_flag)
+                control_alarm(emergency_flag)
+        except Exception as e:
+            print("Error:", e)
+        update_led()
+        utime.sleep(1)  
 
---
+++ pico_alarm/pico_web_server.py
@@ -0,0 +1,70 @@
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor, pico_led
+import machine
+
+def open_socket(ip):
+    """
+    Opens a socket on the specified IP address.
+
+    Args:
+    - ip (str): The IP address to open the socket on.
+
+    Returns:
+    - connection (socket): The opened socket.
+    """
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    print(connection)
+    return connection
+
+    
+def get_pass():
+    """
+    Prompts the user to enter the WiFi SSID and password.
+
+    Returns:
+    - ssid (str): The WiFi SSID entered by the user.
+    - password (str): The WiFi password entered by the user.
+    """
+    while True:
+        ssid = input("Enter your WiFi ssid: ")
+        password = input("Enter your WiFi password: ")
+        return ssid, password
+           
+            
+def connect():
+    """
+    Connects to a WiFi network.
+
+    Returns:
+    - ip (str): The IP address of the connected WiFi network.
+    """
+    # Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    
+    while True:
+        ssid, password = get_pass()
+        wlan.connect(ssid, password)
+        
+        while wlan.isconnected() == False:
+            print('Waiting for connection...')
+            sleep(1)
+        if wlan.isconnected():
+            break
+        
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    return ip
+
+try:
+    ip = connect()
+    connection = open_socket(ip)
+except KeyboardInterrupt:
+    machine.reset()
+
