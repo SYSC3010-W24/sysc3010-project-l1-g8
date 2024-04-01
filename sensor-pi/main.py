f7f0e9681809c1d74250b0e8ce7337fad8400a52
Matteo Golin <matteo.golin@gmail.com>
2024-04-01 15:00:39 -0400

    feat: added class for handling thresholds.

--- sensor-pi/main.py
+++ sensor-pi/main.py
@@ -5,6 +5,7 @@ periodically uploads the recorded data to Firebase, reads user settings, checks
 for data above thresholds and sends UDP messages to other system nodes when an
 emergency has been activated or deactivated.
 """
+
 __author__ = "Matteo Golin"
 
 from types import FrameType
@@ -22,6 +23,7 @@ from signal import signal, SIGTERM
 from smoke_sensor import SmokeSensor
 import time
 from messages import Messages
+from settings import Thresholds
 
 FIREBASE_CONFIG: str = "firebase_config.json"
 UDP_SEND_PORT: int = 2003
@@ -41,9 +43,7 @@ def collect_data(temp: Queue, smoke: Queue) -> None:
     sensehat = SenseHat()
 
     # Create smoke sensor instance
-    smoke_detector = SmokeSensor(
-        sclk=SCLK, miso=MISO, mosi=MOSI, chip_select=CHIP_SELECT
-    )
+    smoke_detector = SmokeSensor(sclk=SCLK, miso=MISO, mosi=MOSI, chip_select=CHIP_SELECT)
 
     while True:
         time.sleep(0.5)  # Don't burn through CPU
@@ -55,26 +55,12 @@ def collect_data(temp: Queue, smoke: Queue) -> None:
         smoke_ppm = smoke_detector.read_ppm()
 
         # Give timestamp for measurements
-        timestamp = (
-            dt.datetime.now().isoformat().replace(".", "+")
-        )  # Remove . because Firebase doesn't allow it
+        timestamp = dt.datetime.now().isoformat().replace(".", "+")  # Remove . because Firebase doesn't allow it
         temp.put((timestamp, temperature))  # Put data on shared queue
         smoke.put((timestamp, smoke_ppm))
 
 
-def get_temperature_threshold(db: Database) -> float:
-    """Gets the configured temperature threshold from the Firebase database."""
-    return float(db.child("thresholds").get("temperature").val().get("temperature"))  # type: ignore
-
-
-def get_smoke_threshold(db: Database) -> float:
-    """Gets the configured smoke PPM threshold from the Firebase database."""
-    return float(db.child("thresholds").get("smoke").val().get("smoke"))  # type: ignore
-
-
-def get_latest_timeout(
-    current_timeout: Optional[dt.datetime], db: Database
-) -> tuple[bool, dt.datetime, int]:
+def get_latest_timeout(current_timeout: Optional[dt.datetime], db: Database) -> tuple[bool, dt.datetime, int]:
     """
     Returns the latest configured timeout from the database and a boolean which describes whether or not the timeout has
     changed in comparison to the currently active one.
@@ -82,9 +68,7 @@ def get_latest_timeout(
     data = db.child("timeout").order_by_key().limit_to_last(1).get().val()
     latest_timeout_timestamp = list(data.keys())[0]  # type: ignore
     duration = list(data.values())[0]  # type: ignore
-    latest_timeout = dt.datetime.strptime(
-        latest_timeout_timestamp, TIMESTAMP_FORMAT
-    )
+    latest_timeout = dt.datetime.strptime(latest_timeout_timestamp, TIMESTAMP_FORMAT)
 
     if current_timeout is not None:
         timeout_changed = latest_timeout > current_timeout
@@ -101,15 +85,9 @@ def shutdown(sig: int, frame: FrameType) -> None:
         exit(0)
 
 
-temp_threshold = float("inf")
-smoke_threshold = float("inf")
-current_timer = None
-
-
 def main() -> None:
     """Runs the primary logic of the sensor data fetcher."""
 
-    global temp_threshold, smoke_threshold, current_timer
     # Connect using configuration
     with open(FIREBASE_CONFIG, "r") as file:
         config = json.loads(file.read())
@@ -121,24 +99,13 @@ def main() -> None:
     db.child("devices").child("sensor-pi").set(ip_addr)
 
     # Check for configured thresholds
-    temp_threshold = get_temperature_threshold(db)
-    smoke_threshold = get_smoke_threshold(db)
+    thresholds = Thresholds.from_db(db)
 
     db.child("emergency").set(False)  # Start with emergency disabled
 
     # Get the latest timeout configuration to start the timeout
     _, current_timeout, _ = get_latest_timeout(None, db)
-    current_timer = None
-
-    # This function is too large to fit in a lambda but needs access to threshold variables
-    def refresh_thresholds() -> tuple:
-        """Refresh the thresholds when the timer expires."""
-        global temp_threshold, smoke_threshold, current_timer
-        temp_threshold = get_temperature_threshold(db)
-        smoke_threshold = get_smoke_threshold(db)
-        current_timer = None
-        print("TIMER WENT OFF")
-        return temp_threshold, smoke_threshold, current_timer
+    current_timer: Timer = Timer(1, thresholds.update)
 
     # Create socket for sending
     channel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -150,10 +117,9 @@ def main() -> None:
     # Handle shutdown signal
     signal(SIGTERM, shutdown)  # type: ignore
 
-    # Start data collection
+    # Start data collection with message queues
     temp = Queue()
     smoke = Queue()
-
     data_collection = Process(target=collect_data, args=(temp, smoke))
     data_collection.start()
 
@@ -163,35 +129,27 @@ def main() -> None:
         timestamp, smoke_ppm = smoke.get()
 
         # Alert of emergency if threshold is exceeded or if there is smoke
-        if temperature > temp_threshold or smoke_ppm > smoke_threshold:
+        if thresholds.temperature_exceeded(temperature) or thresholds.smoke_exceeded(smoke_ppm):
             db.child("emergency").set(True)
             alarm_ip = db.child("devices").child("alarm").get().val()
-            channel.sendto(
-                Messages.EMERGENCY.value.to_bytes(), (alarm_ip, UDP_SEND_PORT)
-            )
+            channel.sendto(Messages.EMERGENCY.value.to_bytes(), (alarm_ip, UDP_SEND_PORT))
             local_emergency = True
 
         # Write measurement to database
-        db.child("sensordata").child("temperature").child(timestamp).set(
-            temperature
-        )
+        db.child("sensordata").child("temperature").child(timestamp).set(temperature)
         db.child("sensordata").child("smoke").child(timestamp).set(smoke_ppm)
 
         # Update configuration unless there's a timeout
-        print(temp_threshold, smoke_threshold)
-        if current_timer is None:
-            temp_threshold = get_temperature_threshold(db)
-            smoke_threshold = get_smoke_threshold(db)
+        if not current_timer.is_alive():
+            thresholds.update(db)
 
         # Check for timeout
-        timeout_changed, current_timeout, duration = get_latest_timeout(
-            current_timeout, db
-        )
+        timeout_changed, current_timeout, duration = get_latest_timeout(current_timeout, db)
 
         if timeout_changed:
             print("TIMER RECEIVED")
             # If a timer is already running, stop it
-            if current_timer is not None:
+            if current_timer.is_alive():
                 current_timer.cancel()
 
             time_expired = (dt.datetime.now() - current_timeout).total_seconds()
@@ -199,12 +157,10 @@ def main() -> None:
 
             # Make sure timer is not too old to be relevant
             if actual_duration > 0:
-                current_timer = Timer(actual_duration, refresh_thresholds)
+                current_timer = Timer(actual_duration, thresholds.update)
 
                 # Make thresholds so high that nothing will happen
-                temp_threshold = float("inf")
-                smoke_threshold = float("inf")
-
+                thresholds.max_out()
                 current_timer.start()  # Start timer
 
         # Check for differing emergency status in database (user deactivated)
@@ -213,9 +169,7 @@ def main() -> None:
 
             # Notify other nodes that emergency is over
             alarm_ip = db.child("devices").child("alarm").get().val()
-            channel.sendto(
-                Messages.NO_EMERGENCY.value.to_bytes(), (alarm_ip, UDP_SEND_PORT)
-            )
+            channel.sendto(Messages.NO_EMERGENCY.value.to_bytes(), (alarm_ip, UDP_SEND_PORT))
 
 
 if __name__ == "__main__":
