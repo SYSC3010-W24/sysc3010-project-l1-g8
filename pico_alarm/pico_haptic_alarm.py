8c6b9adac31424e4368b81c0dd8f9fe6bd83fb03
javeriasohail123 <javeriasohail@cmail.carleton.ca>
2024-04-01 16:17:01 -0400

    feat: final changes to pico_haptic_alarm.py with flake8

--- pico_alarm/pico_haptic_alarm.py
+++ pico_alarm/pico_haptic_alarm.py
@@ -1,11 +1,33 @@
 import machine
 import utime
-import urequests as requests
-import json
+import urequests
+import requests
+import network
 from picozero import LED
 
+def do_connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network')
+        wlan.connect("bell511", 'javeriamnah@23')
+        print('....')
+        while not wlan.isconnected():
+            print('Waiting for connection...')
+            utime.sleep(1)
+    print('connected to network', wlan.ifconfig()[0])
+
 # Firebase database URL
-# firebase_url = "https://fans-38702-default-rtdb.firebaseio.com/"
+firebase_url = 'https://fans-38702-default-rtdb.firebaseio.com//emergency.json'
+auth_data = {
+    "email": "javeria1228@gmail.com",
+    "password": "firebase123!",
+    "returnSecureToken": True
+}
+auth_response = requests.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDCrm-YWek1mShoftACTezFdzn8PoLSNrY", json=auth_data)
+auth_response_data = auth_response.json()
+print(auth_response_data)
+auth_response.close()
 
 # Connects buzzer to PicoW
 buzzer_pin = machine.Pin(1)
@@ -18,7 +40,7 @@ red_led = LED(6)
 # Connects button to PicoW
 stop_button = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_UP)
 
-# Frequencies (Hz) for alarm sound
+# Frequencies (Hz) for alarm sound 
 low_freq = 400
 high_freq = 1000
 
@@ -26,86 +48,71 @@ high_freq = 1000
 last_emergency_time = 0
 emergency_duration = 0
 emergency_threshold = 30000  # 30 seconds
-emergency_flag = True
 emergency_local = True
 
-
-def get_emergency_flag():
-    response = requests.get(firebase_url)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data
-    return None
-
-
 def control_alarm():
-    global buzzer_pwm
+    global buzzer_pwm, emergency_flag, emergency_local
     if emergency_flag and emergency_local:
         # Start buzzing
         update_led()
-        for _ in range(200):  # Play the alarm sound for 200 milliseconds
-            buzzer_pwm.freq(low_freq)
-            buzzer_pwm.duty_u16(32768)  # 50% duty cycle
-            utime.sleep_ms(100)
-
-            buzzer_pwm.freq(high_freq)
-            utime.sleep_ms(100)
+        while emergency_flag:
+            # Buzz for 200 milliseconds
+            if emergency_flag:  # Check if emergency flag is still True
+                buzzer_pwm.freq(low_freq) 
+                buzzer_pwm.duty_u16(32768)  # 50% duty cycle
+                utime.sleep(0.02)
+            # Switch frequency to high for buzzing
+            if emergency_flag:  # Check if emergency flag is still True
+                buzzer_pwm.freq(high_freq)
+                utime.sleep(0.02)
             if stop_button.value() == 0:
-                button = True
+                # Button pressed, indicating awareness of the alarm
+                emergency_local = False
+                update_led()
                 break
-    else:
-        # Stop buzzing
+    else: # Stop buzzing
         buzzer_pwm.duty_u16(0)
-        buzzer_pwm.freq(100)
         update_led()  # Update LED to reflect emergency flag state
-
-
+    
 def update_led():
-    global last_emergency_time, emergency_duration, emergency_threshold, emergency_flag, emergency_local
-    current_time = utime.ticks_ms()
-
+    global emergency_flag, emergency_local, button
     if emergency_flag and not emergency_local:
         # If emergency, turn on red LED and turn off green LED
-        red_led.on()
+        red_led.blink(on_time=0.5, off_time=0.5)
+        #red_led.on()
         green_led.off()
-        if button and emergency_flag:
-            red_led.blink(on_time=0.5, off_time=0.5)
-            buzzer_pwm.duty_u16(0)
-            buzzer_pwm.freq(100)
-
+    if not emergency_flag:
+        emergency_local= True
+        red_led.off()
+        green_led.on()
     elif emergency_flag and emergency_local:
         # If emergency, turn on red LED and turn off green LED
         red_led.on()
         green_led.off()
-        if button and emergency_flag:
-            red_led.blink(on_time=0.5, off_time=0.5)
-
-    elif not emergency_flag and not emergency_local:
-        # Otherwise, turn off red LED and turn on green LED
-        red_led.off()
-        green_led.on()
-
+        
 
 if __name__ == "__main__":
-    global button
-    button = False
+    button = False  # Global variable
+    # Establish network connection
+    do_connect()
     # Main loop
-    green_led.on()  # Turn on green LED by default
+    green_led.on() # Turn on green LED by default
     red_led.off()  # Turn off red LED by default
-    # emergency_flag = get_emergency_flag()
-
-    # if emergency_flag is not None:
-    #    print("Emergency flag:", emergency_flag)
-    # else:
-    #    print("Failed to fetch emergency flag")
-
     while True:
         try:
             if stop_button.value() == 0:
                 button = True
                 emergency_local = False
                 print("Stop button pressed!")
-            control_alarm()
+            while True:    
+                response=urequests.get(firebase_url)
+                data=response.json()
+                response.close()
+                print(data)
+                emergency_flag = data
+                control_alarm()
         except Exception as e:
             print("Error:", e)
         utime.sleep(1)
+
+
