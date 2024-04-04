043d4096b95e17930eee0c86bd5916d209f6c072
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-04-04 09:12:10 -0400

    style: Format all html, css, and js documents

--- web-app/static/script.js
+++ web-app/static/script.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const darkModeSwitch = document.getElementById("darkModeSwitch");
-  const currentTheme = localStorage.getItem("theme") || "light";
-  document.documentElement.setAttribute("data-theme", currentTheme);
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeSwitch = document.getElementById("darkModeSwitch");
+    const currentTheme = localStorage.getItem("theme") || "dark";
+    document.documentElement.setAttribute("data-theme", currentTheme);
 
-  function toggleDarkMode() {
-      const isDarkMode = document.documentElement.getAttribute("data-theme") === "dark";
+    function toggleDarkMode() {
+        const isDarkMode = document.documentElement.getAttribute("data-theme") === "dark";
 
-      if (isDarkMode) {
-          document.documentElement.setAttribute("data-theme", "light");
-          localStorage.setItem("theme", "light");
-      } else {
-          document.documentElement.setAttribute("data-theme", "dark");
-          localStorage.setItem("theme", "dark");
-      }
-  }
+        if (isDarkMode) {
+            document.documentElement.setAttribute("data-theme", "light");
+            localStorage.setItem("theme", "light");
+        } else {
+            document.documentElement.setAttribute("data-theme", "dark");
+            localStorage.setItem("theme", "dark");
+        }
+    }
 
-  darkModeSwitch.addEventListener("click", toggleDarkMode);
+    darkModeSwitch.addEventListener("click", toggleDarkMode);
 });
