45d11e55f11d37309f279cfb8f6b21e836bf4a3c
SajaFawagreh <115193073+SajaFawagreh@users.noreply.github.com>
2024-04-04 09:51:32 -0400

    feat: resolved flake8 issues

--- notifier/messages.py
+++ notifier/messages.py
@@ -7,7 +7,8 @@ from enum import IntEnum
 
 
 class Messages(IntEnum):
-    """Represents the types of messages that are possible to received over UDP."""
+    """Represents the types of messages that
+    are possible to received over UDP."""
 
     EMERGENCY = 0
     EMERGENCY_OVER = 1
